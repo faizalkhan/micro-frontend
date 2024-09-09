85e730196ec3ed80b3782041d09647656407b103
Faizal Shahul Hamed <fsl@tpcdevsvr.com>
2024-09-09 15:42:03 +0530

    Initial Code

---
+++ JavaScript/container/webpack.config.js
@@ -0,0 +1,25 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+module.exports = {
+         mode: "development",
+         devServer: 
+         {
+             port: 1000
+         },
+         plugins : [
+
+            new ModuleFederationPlugin({
+                name : 'container',                
+                remotes: {
+                    barchart : 'barchart@http://localhost:1001/remoteEntry.js',
+                    linechart: 'linechart@http://localhost:1003/remoteEntry.js'
+                }
+
+                }),
+
+                 new HtmlWebpackPlugin({
+                template : "./public/index.html",
+            }),
+         ],
+}
---
+++ JavaScript/linechart/src/index.js
@@ -0,0 +1,21 @@
+
+
+import * as echarts from "echarts";
+
+var myLineChart = echarts.init(document.getElementById('linechart-dev'));
+
+myLineChart.setOption({
+    xAxis: {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    },
+    yAxis: {
+      type: 'value'
+    },
+    series: [
+      {
+        data: [150, 230, 224, 218, 135, 147, 260],
+        type: 'line'
+      }
+    ]
+  });
